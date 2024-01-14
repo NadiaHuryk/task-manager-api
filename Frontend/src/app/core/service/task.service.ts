1340f2cce247749c444b4ec6fafe307874d12fea
Nadiia Huryk <121286840+NadiaHuryk@users.noreply.github.com>
2024-01-14 15:02:32 +0200

    add folders, added constants for URL in services (#8)
    * fixed some issues
    
    * add folders, added constants for URL in services

--- Frontend/src/app/core/service/task.service.ts
+++ Frontend/src/app/core/service/task.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Task } from "../model/task";
+import { environment } from "../../environments/environments";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private baseUrl = 'http://localhost:8080/tasks';
+  private baseUrl = `${environment.apiUrl}/tasks`;
 
   constructor(private http: HttpClient) {}
 
